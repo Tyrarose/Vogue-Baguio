d06cedfbf5140170bc6d270d73a9c73502fd5f1f
Tyra <68145311+Tyrarose@users.noreply.github.com>
2024-03-24 21:07:09 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import fs from "fs/promises";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+let data;
+
+async function loadArticles() {
+	try {
+		const rawData = await fs.readFile("articles.json", "utf8");
+		return JSON.parse(rawData);
+	} catch (error) {
+		console.error("Error reading the JSON file:", error);
+		return []; // Return an empty array in case of error
+	}
+}
+
+// Home page that lists articles
+app.get("/", async (req, res) => {
+	const articles = await loadArticles();
+	const mainFeaturedArticle = articles.find((article) => article.id === "1");
+	res.render("index.ejs", {
+		articles: articles,
+		mainFeaturedArticle: mainFeaturedArticle,
+	});
+});
+
+// Individual post page
+app.get("/post/:id", async (req, res) => {
+	const articles = await loadArticles();
+	const article = articles.find((article) => article.id === req.params.id);
+	if (article) {
+		res.render("article.ejs", { article: article });
+	} else {
+		res.status(404).send("Article not found");
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server on ${port}`);
+});
